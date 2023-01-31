057d4ee4cf03accd639f0141d18703625dcf5a22
Corvo <lucascoutojp@gmail.com>
2023-01-31 22:46:50 +0900

    we modified the image model and created a link between the category table and the post table

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'label',
+        'slug',
+        'type'
+    ];
+
+    public function image_post()
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
